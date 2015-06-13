a58555fd024fa5955e30cd2143dcb8833acd21fa
Oliver Matthews <ojm@codersoffortune.net>
2015-06-13 21:44:53 +0100

    load octgn files via the gui

--- ttanr_gui.py
+++ ttanr_gui.py
@@ -10,11 +10,33 @@ class TTANR:
     def __init__(self,root):
         self.root=root
         self.outDir= os.path.join(os.path.expanduser("~"),"Documents")
+
+        inputFrame=Tk.Frame(root)
+        inputFrame.pack()
+        Tk.Label(inputFrame,text="Select input source")
+        self.chooseO8N=Tk.BooleanVar()
+        o8nRButton=Tk.Radiobutton(inputFrame,text="OCTGN File",variable=self.chooseO8N,value=True,command=self.inputO8N)
+        ndbRButton=Tk.Radiobutton(inputFrame,text="netrunnerdb",variable=self.chooseO8N,value=False,command=self.inputNDB)
+
+        o8nRButton.pack(side=Tk.LEFT)
+        ndbRButton.pack(side=Tk.LEFT)
+
+        o8nFrame=Tk.Frame(root)
+        o8nFrame.pack()
+        Tk.Label(o8nFrame,text="Select file to import").pack()
+        self.o8nEntry=Tk.Entry(o8nFrame)
+        self.o8nEntry.pack(side=Tk.LEFT)
+        self.o8nButton=Tk.Button(o8nFrame,text="Browse",command=self.pickO8NFile)
+        self.o8nButton.pack(side=Tk.LEFT)
+        
         numberFrame = Tk.Frame(root)
         numberFrame.pack()
         Tk.Label(numberFrame,text="Enter netrunnerdb # to import").pack()
         self.deckEntry=Tk.Entry(numberFrame)
         self.deckEntry.pack()
+
+        ndbRButton.select()
+        ndbRButton.invoke()
 
         outputDirFrame = Tk.Frame(root)
         outputDirFrame.pack()
@@ -52,7 +74,27 @@ class TTANR:
             self.urlEntry.config(state=Tk.NORMAL)
         else:
             self.urlEntry.config(state=Tk.DISABLED)
-        
+
+    def inputO8N(self):
+        self.o8nEntry.config(state=Tk.NORMAL)
+        self.o8nButton.config(state=Tk.NORMAL)
+        self.deckEntry.config(state=Tk.DISABLED)
+
+    def inputNDB(self):
+        self.o8nEntry.config(state=Tk.DISABLED)
+        self.o8nButton.config(state=Tk.DISABLED)
+        self.deckEntry.config(state=Tk.NORMAL)
+
+    def pickO8NFile(self):
+        self.deckFile=tkFileDialog.askopenfilename(
+            parent=self.root,
+            initialdir=os.path.join(os.path.expanduser("~"),"Downloads"),
+            filetypes=[('OCTGN files','*.o8d')],
+            defaultextension='o8d',
+            title='Choose deck file')
+        self.o8nEntry.delete(0,Tk.END)
+        self.o8nEntry.insert(0,self.deckFile)
+    
     def pickOutputDir(self):
         self.outDir=tkFileDialog.askdirectory(
             parent=self.master,
@@ -64,11 +106,17 @@ class TTANR:
 
     def go(self):
         deckID=self.deckEntry.get()
-        deck=ttanr.load_netrunnerdb_deck(deckID)
+        deck=None
+        if self.chooseO8N:
+            deck=ttanr.load_octgn_deck(self.deckFile)
+        else:
+            deck=ttanr.load_netrunnerdb_deck(deckID)
+
         if self.useUrl.get():
             ttanr.write_files(deck,self.urlEntry.get(),self.install.get())
         else:
             ttanr.write_files(deck,"null://",self.install.get())
+
         tkMessageBox.showinfo("ttanr","Done")
 
 
