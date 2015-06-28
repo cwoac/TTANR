67cf86f7ab4ac2ba872f68052ad60115da371763
Oliver Matthews <ojm@codersoffortune.net>
2015-06-28 10:58:23 +0100

    cli support for whi

--- ttswhi.py
+++ ttswhi.py
@@ -3,6 +3,9 @@
 import ttsutil
 import urllib
 import BeautifulSoup
+import os.path
+import PIL.Image
+import argparse
 
 cards={}
 debug=True
@@ -16,16 +19,32 @@ def make_filename(image):
 def make_cache_dir():
   ttsutil.make_cache_dir(game)
 
+def get_card(id):
+  filename=make_filename(id+".jpg")
+  if not os.path.isfile(filename):
+    make_cache_dir()
+    print("Downloading card: %s" % id)
+    data=urllib.urlopen("http://deckbox.org/whi/%s" % id).read()
+    soup=BeautifulSoup.BeautifulSoup(data)
+    url="http://deckbox.org/%s" % soup.findAll('img',{'id':'card_image'})[0]['src']
+    image=urllib.urlopen(url).read()
+    fh=open(filename,'wb')
+    fh.write(image)
+    fh.close()
+
 def load_deckbox_deck(id):
   print("Attempting to load deck %s from deckbox" % id)
-  # TODO: read name better
-  name = "Deck id %s" % id
+  html=urllib.urlopen("http://deckbox.org/sets/%s" % id).read()
+  soup=BeautifulSoup.BeautifulSoup(html)
+  name = soup.head.title.contents[0].replace("&#x27;","'").split(" - ")[0]
+  print("Found deck '%s'" % name )
   deck={
     'name':name,
     'cards':[],
-    'filename':ttsutil.sanitise_filename(name)
+    'filename':ttsutil.sanitise_filename(name),
+    'back_filename':None
   }
-
+  print("Loading deck contents")
   data=urllib.urlopen("http://deckbox.org/sets/%s/export" % id).read()
   soup=BeautifulSoup.BeautifulSoup(data)
   for line in soup.body.childGenerator():
@@ -35,11 +54,46 @@ def load_deckbox_deck(id):
     if len(line)==0:
       continue
     count=int(line[0])
-    name=line[1:]
-    print( "%s (x%s)" % (name,count) )
+    name=line[1:].strip()
+    get_card(name)
     deck['cards'].append((name,count))
   return deck
 
-x=load_deckbox_deck(1007109)
+def write_files(deck,base_url,write_local,local_target,install):
+  chest=ttsutil.build_chest_file(deck,base_url)
+  back_image=None
+  if deck['back_filename']:
+    print "loading custom back %s" % deck['back_filename']
+    back_image=ttsutil.load_image_at_size(deck['back_filename'])
+  else:
+    back_image=PIL.Image.new('RGBA',(ttsutil.imgW,ttsutil.imgH),(0,255,0,255))
+  ttsutil.write_files(deck,chest,base_url,write_local,local_target,install,back_image,game)
 
-print x
+def main():
+  parser = argparse.ArgumentParser(description="Create a set of files for loading WHI decks into TableTop Simulator")
+  parser.add_argument("-d","--deckbox",metavar="ID",help="Load deck from deckbox.org using given ID.")
+  parser.add_argument("-b","--back",metavar="backgroundFile",help="Override default back with given file.")
+  parser.add_argument("-i","--install",action="store_true",help="Install files into local TTS install.")
+  parser.add_argument("-w","--writelocal",action="store_true",help="Write files into the current directory.")
+  parser.add_argument("-u","--url",help="Base url for where the images will be made availiable")
+  args = parser.parse_args()
+
+  if not (args.install or args.url):
+    print("Warning: Neither install (-i) or url (-u) has been specified. You probably don't want to do this.")
+
+  if not (args.install or args.writelocal):
+    parser.error("At least one of -i or -w is required.")
+
+  baseurl=args.url or "null://"
+
+  if baseurl.startswith('file') and not baseurl.endswith(os.sep):
+    baseurl+=os.sep
+  if baseurl.startswith('http') and not baseurl.endswith('/'):
+    baseurl+='/'
+
+  deck=load_deckbox_deck(args.deckbox)
+  deck['back_filename']=args.back
+  write_files(deck,baseurl,args.writelocal,"",args.install)
+
+if __name__ == "__main__":
+    main()
